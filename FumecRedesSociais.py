f27c29539549961cf8c37c0446f83b153236cd80
dfalci <danielfalci@gmail.com>
2017-01-24 08:52:58 -0200

    Add files via upload

---
+++ FumecRedesSociais.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+# File name: Calculos.py
+# Author: Daniel Henrique Mourao Falci
+# Date created: 23/11/2016
+# Python Version: 2.7
+
+import networkx as nx
+from matplotlib import pyplot as plt
+import numpy as np
+import math
+from collections import Counter
+
+class FUMECRedesSociais:
+
+    def __init__(self, file):
+        self.file = file
+        self.g = None
+
+    def calcularRedeAleatoria(self, vertices, arestas):
+        """
+        :param vertices:
+        :param arestas:
+        :return:
+        """
+        p = float(2 * arestas) / float(vertices * (vertices -1))
+
+        print 'P : '+str(p)
+
+        grafo = nx.erdos_renyi_graph(vertices, p)
+        print 'C : '+str(nx.average_clustering(grafo))
+        print 'L : '+str(nx.average_shortest_path_length(grafo))
+
+
+    def load(self):
+        """
+        nao faca a exportacao completa no gephi, exporte apenas os atributos
+
+        se nao carregar - substituir as primeiras tags do xml set que quer carregar por estas linhas abaixo
+
+        #<?xml version='1.0' encoding='utf-8'?>
+        #<gexf version="1.1" xmlns="http://www.gexf.net/1.1draft" xmlns:viz="http://www.gexf.net/1.1draft/viz" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.w3.org/2001/XMLSchema-instance">
+
+        alem disso, deve-se remover a tag meta
+
+        """
+        self.g = nx.read_gexf(self.file)
+
+    def scaleFree(self, tipo='GRAU', inclinacaoAjuste=-2.1, interceptoAjuste=8.8, limiteY=None, limiteX=None):
+        data = ()
+        if (tipo == 'GRAU'):
+            data = tuple(self.g.node[este]['Grau'] for este in self.g.node)
+        elif tipo == 'GRAU DE ENTRADA':
+            data = tuple(self.g.node[este]['Grau de entrada'] for este in self.g.node)
+        else :
+            data = tuple(self.g.node[este][u'Grau de saída'] for este in self.g.node)
+        res = Counter(data)
+        graus = []
+        contagem = []
+        for este in res.iteritems():
+            if (este[0]>0):
+                graus.append(math.log(este[0]))
+                contagem.append(math.log(este[1]))
+        print res
+
+        #faz a regressao
+        regressao = np.polyfit(graus, contagem, 1)
+        inclinacao = regressao[0]
+        intercepto = regressao[1]
+        print 'inclinacao '+str(inclinacao)
+        print 'intercepto '+str(intercepto)
+
+        predicao = intercepto + inclinacao * np.array(graus)
+
+        print 'x1:'+str(graus[0])+' - x2:'+str(graus[len(graus)-1])
+        print 'y1:'+str(predicao[0])+' - y2:'+str(predicao[len(predicao)-1])
+
+        valorMaximo = np.array(contagem).max()
+        print 'Valor maximo : '+str(valorMaximo)
+
+        fig = plt.figure(2, figsize=(6, 6), dpi=80)
+        plt.subplot(2,1,1)
+        plt.scatter(res.keys(), res.values(), c='r', marker='s')
+        plt.xlabel('Grau')
+        plt.ylabel('Frequencia')
+        plt.title('a) Distribuicao dos graus')
+        if (limiteY != None):
+            plt.ylim((-1, limiteY))
+        if (limiteX != None):
+            plt.xlim((-1, limiteX))
+        plt.grid(True)
+        plt.draw()
+
+        plt.subplot(2,1,2)
+        plt.plot(graus, contagem, c='r', marker='s', linestyle='None')
+        plt.plot(graus, predicao, c='b', lw=2.5)
+        plt.plot(graus, interceptoAjuste + inclinacaoAjuste * np.array(graus), c='green', lw=2.5)
+        plt.grid(True)
+        plt.ylabel('Log(Frequencia)')
+        plt.xlabel('Log(Grau)')
+        plt.legend(['Dados', r'Regressao $\alpha = '+'{0:.3f}'.format(inclinacao)+'$', r'Ajustado $\alpha = '+'{0:.3f}'.format(inclinacaoAjuste)+'$'])
+        plt.yscale('linear')
+        plt.xscale('linear')
+        plt.ylim((-0.1,interceptoAjuste+0.3))
+        plt.xlim((-0.1, 7))
+        plt.title('b) Grafico Log-Log')
+
+        plt.show()
+
+o = FUMECRedesSociais("test.gexf") # gexfFile deve estar na versão 1.0
+o.load()
+o.scaleFree('GRAU', -2.8, 9.1)
+#o.scaleFree('GRAU DE ENTRADA', -1.9, 8.3)
+#o.scaleFree('GRAU DE SAIDA', -1.9, 8.5)
+#o.calcularRedeAleatoria(1589, 2742)
+
+#P : 0.00233962636533
+#C : 0.00185746359891
+#L : 3.90169624401
---
+++ GraphGenerator.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+import networkx as nx
+import community
+
+class NetworkHandle:
+
+    def __init__(self, file):
+        self.file = file
+        if (file != None):
+            self.g = nx.read_gexf(self.file)
+
+    def __calcularGrauGeralPorModulo(self, vertice, nomeEntrada, nomeSaida, resultado):
+        vertice[resultado] = vertice[nomeEntrada] + vertice[nomeSaida]
+
+    def __getProp(self, vertice, prop, default):
+        try:
+            return vertice[prop]
+        except:
+            vertice[prop] = default
+            return vertice[prop]
+
+    def __incrementarProp(self, vertice, prop, valor):
+        item = self.__getProp(vertice, prop, 0)
+        vertice[prop] = item + valor
+
+    def __setProp(self,vertice, prop, valor):
+        vertice[prop] = valor
+
+    def calculateModularityClasses(self):
+        parts = community.best_partition(nx.DiGraph.to_undirected(self.g))
+        print parts
+
+
+    def __calculateModularityDegree(self):
+        #zera os valores antes do calculo
+        for este in self.g.node:
+            vertice = self.g.node[este]
+            self.__setProp(vertice, 'GrauEntradaInterno', 0)
+            self.__setProp(vertice, 'GrauSaidaInterno', 0)
+            self.__setProp(vertice, 'GrauEntradaExterno', 0)
+            self.__setProp(vertice, 'GrauSaidaExterno', 0)
+            self.__setProp(vertice, 'GrauExterno', 0)
+            self.__setProp(vertice, 'GrauInterno', 0)
+
+        #calcula os graus de entrada e saida internos e externos
+        for origem in self.g.edge:
+            arestas = self.g.edge[origem]
+            for entrada in arestas:
+                originVertex = self.g.node[origem]
+                destinationVertex = self.g.node[entrada]
+                if originVertex['Modularity Class'] == destinationVertex['Modularity Class']:
+                    self.__incrementarProp(originVertex, 'GrauSaidaInterno', 1)
+                    self.__incrementarProp(destinationVertex, 'GrauEntradaInterno', 1)
+                if originVertex['Modularity Class'] != destinationVertex['Modularity Class']:
+                    self.__incrementarProp(originVertex, 'GrauSaidaExterno', 1)
+                    self.__incrementarProp(destinationVertex, 'GrauEntradaExterno', 1)
+
+        #calcula os graus internos e externos
+        for este in self.g.node:
+            vertice = self.g.node[este]
+            self.__calcularGrauGeralPorModulo(vertice, 'GrauEntradaInterno', 'GrauSaidaInterno', 'GrauInterno')
+            self.__calcularGrauGeralPorModulo(vertice, 'GrauEntradaExterno', 'GrauSaidaExterno', 'GrauExterno')
+
+
+    def __toLog(self, number):
+        #converte para log o valor se for necessário...
+        #if (number != None and number > 0):
+        #    return float(math.log10(number))
+        #return number
+        return number
+
+
+    def __calculate(self, vertex):
+        intermediacao = vertex['Betweenness Centrality']
+        pageRank = vertex['PageRank']
+        grauDeEntrada = vertex['Grau de entrada']
+        grauEntradaInterno = vertex['GrauEntradaInterno']
+        grauSaidaInterno = vertex['GrauSaidaInterno']
+        grauEntradaExterno = vertex['GrauEntradaExterno']
+        grauSaidaExterno = vertex['GrauSaidaExterno']
+        grauDeSaida = vertex[u'Grau de saída']
+        grau = vertex['Grau']
+        eigenVector = vertex['Eigenvector Centrality']
+        authority = vertex['Authority']
+        hub = vertex['Hub']
+        vertex['grupo'] = vertex['Modularity Class']
+
+        vertex['m_betweenXPageRank'] = self.__toLog(intermediacao * pageRank)
+        vertex['m_betweenXEignv'] = self.__toLog(eigenVector * intermediacao)
+
+        vertex['m_indegXPageRank'] = self.__toLog(grauDeEntrada * pageRank)
+        vertex['m_indegXEign'] = self.__toLog(grauDeEntrada * eigenVector)
+        vertex['m_indegXBetween'] = self.__toLog(grauDeEntrada * intermediacao)
+
+        vertex['m_indegInternoXPageRank'] = self.__toLog(grauEntradaInterno * pageRank)
+        vertex['m_indegInternoXEign'] = self.__toLog(grauEntradaInterno * eigenVector)
+        vertex['m_indegInternoXBetween'] = self.__toLog(grauEntradaInterno * intermediacao)
+
+        vertex['m_outDegInternoXPageRank'] = self.__toLog(grauSaidaInterno * pageRank)
+        vertex['m_outDegInternoXEign'] = self.__toLog(grauSaidaInterno * eigenVector)
+        vertex['m_outDegInternoXBetween'] = self.__toLog(grauSaidaInterno * intermediacao)
+
+        vertex['m_indegExternoXPageRank'] = self.__toLog(grauEntradaExterno * pageRank)
+        vertex['m_indegExternoXEign'] = self.__toLog(grauEntradaExterno * eigenVector)
+        vertex['m_indegExternoXBetween'] = self.__toLog(grauEntradaExterno * intermediacao)
+
+        vertex['m_outDegExternoXPageRank'] = self.__toLog(grauSaidaExterno * pageRank)
+        vertex['m_outDegExternoXEign'] = self.__toLog(grauSaidaExterno * eigenVector)
+        vertex['m_outDegExternoXBetween'] = self.__toLog(grauSaidaExterno * intermediacao)
+
+        vertex['m_outDegXPageRank'] = self.__toLog(grauDeSaida * pageRank)
+        vertex['m_outDegXEign'] = self.__toLog(grauDeSaida * eigenVector)
+        vertex['m_outDegXBetween'] = self.__toLog(grauDeSaida * intermediacao)
+
+        vertex['m_degPageRank'] = self.__toLog(grau* pageRank)
+        vertex['m_degXEign'] = self.__toLog(grau * eigenVector)
+        vertex['m_degXBetween'] = self.__toLog(grau * intermediacao)
+
+
+        vertex['m_authXEign'] = self.__toLog(authority * eigenVector)
+        vertex['m_authXPageRank'] = self.__toLog(authority * pageRank)
+        vertex['m_authXBetween'] = self.__toLog(authority * intermediacao)
+
+    def __save(self):
+        nx.write_gexf(self.g, self.file)
+
+
+    def calcularNossaMetrica(self):
+        self.__calculateModularityDegree()
+        for este in self.g.node:
+            vertice = self.g.node[este]
+            self.__calculate(vertice)
+        self.__save()
+
+
+
+
+
+
+
+l = NetworkHandle('trabalhofinal.gexf')
+#l.calculateModularityClasses()
+l.calcularNossaMetrica()
+
+
